da8c90d8772b8975780adee3516a6ce3bac2556c
es5es5 <es5e5s@nate.com>
2022-02-21 23:26:28 +0900

    cordova

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// import style (<= Swiper 5.x)
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
 Vue.config.productionTip = false
 
 new Vue({
